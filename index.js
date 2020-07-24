0fc14921367144c32c40077e7a1e878903e5639a
Joshua Mba <angelicjoekeyz@gmail.com>
2020-07-24 12:09:12 +0100

    First commit

--- index.js
+++ index.js
@@ -1,41 +1,44 @@
 let week = ["Monday", "Tuesday", "Wednessday", "Thursday", "Friday", "Saturday", "Sunday"];
 
 let activities = [{
-        event: ["Ate Pizza", "Went to Genesys", "Wrote code all Night"]
+        event: ["Ate Pizza", "Went to Genesys Tech Hub", "Wrote code all Night"]
     },
     {
-        event: ["Ate Fried Egg", "Ate Bean", "Wrote code all Night"]
+        event: ["Ate Fried Egg", "Went to Genesys Tech Hub", "Wrote code all Night"]
     },
     {
-        event: ["Ate Bread", "Went to Church", "Visited a Friend"]
+        event: ["Ate Bread", "Went for Medical Checkup", "Visited a Friend"]
     },
 ]
 
 
 let Jacque_log = () => {
+    // loop for each day of the week
     for (w = 0; w < week.length; w++) {
-        // console.log(week[w])
         let week_day = week[w];
         let activity = activities[Math.floor(Math.random() * activities.length)]
         activity = JSON.stringify(activity.event);
-        // console.log(activity);
+
+        //class constructor to analyse Jacque Events
         class Jacque {
+
             constructor(week_day, activity) {
                 this.day = week_day;
                 this.activity = activity;
-                // console.log(this.day)
-                // console.log(this.activity)
             }
+
+            //transform method
             transform() {
+
                 if (this.activity.indexOf("Ate Pizza") > -1) {
                     return "Jacque Transformed to Squirrel";
                 } else {
                     return "Jacque did not Transform to a Squirrel";
-
-
                 }
+
             }
         }
+
         //invoke class and console the result for each day
         let week_log = new Jacque(week_day, activity);
         console.log(`${week_log.day}: 
@@ -44,7 +47,6 @@ let Jacque_log = () => {
 
     }
 }
-
 
 let analyse = () => {
     for (i = 1; i < 3; i++) {
